95ee530d6009950308065ffd56a1827ec3b1da0a
João Vitor Assis <128652207+joaoassis1@users.noreply.github.com>
2023-12-17 17:37:51 -0400

    Exercício utilizando funções para descobrir números em comum nos arrays
    Para começar eu criei dois arrays e adicionei 10 números em cada, depois adicionei uma função para encontrar os números em comum entre esses arrays e ao final ele exibir os números em comum.

---
+++ Dia15.js
@@ -0,0 +1,19 @@
+let arrayA = [13, 20, 10, 22, 24, 20, 27, 17, 19, 14]
+let arrayB = [24, 22, 23, 11, 15, 17, 29, 14, 16, 21]
+
+function NumerosEmComum(arrayUm, arrayDois){
+    let resultado = []
+    let contadorArrayResultado = 0
+
+    for (let i = 0; i < arrayUm.length; i++){
+        for (let j = 0; j < arrayDois.length; j++){
+            if (arrayUm[i] == arrayDois[j]){
+                resultado[contadorArrayResultado] = arrayUm[i]
+                contadorArrayResultado++
+                j = arrayDois.length
+            }
+        }
+    }
+    return resultado
+}
+    console.log(NumerosEmComum(arrayA, arrayB))
